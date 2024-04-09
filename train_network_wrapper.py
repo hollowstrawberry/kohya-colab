5a437393a35cfebbcf3dbc44d28bcc43a1a5d18c
Samuel <33796679+hollowstrawberry@users.noreply.github.com>
2024-04-09 00:09:59 -0400

    fix jax and nicer errors

--- train_network_wrapper.py
+++ train_network_wrapper.py
@@ -1,13 +1,25 @@
-import sys
-import traceback
-from train_network import setup_parser, train
-from library.train_util import read_config_from_file
-
 try:
+    from train_network import setup_parser, train
+    from library.train_util import read_config_from_file
+
     parser = setup_parser()
     args = parser.parse_args()
     args = read_config_from_file(args, parser)
+
     train(args)
+
 except:
-    print(traceback.format_exc())
+    import sys
+    import traceback
+    from pygments import formatters, highlight, lexers
+    from dracula import DraculaStyle
+
+    tb = traceback.format_exc().split("\n")
+    tb_text = "\n".join(tb[:-2])
+
+    lexer = lexers.get_lexer_by_name("pytb", stripall=True)
+    formatter = formatters.Terminal256Formatter(style=DraculaStyle)
+    tb_colored = highlight(tb_text, lexer, formatter)
+
+    print(f"{tb_colored}\n\033[0;31m\033[1m{tb[-2]}\n")
     sys.exit(1)
--- train_network_xl_wrapper.py
+++ train_network_xl_wrapper.py
@@ -1,14 +1,26 @@
-import sys
-import traceback
-from sdxl_train_network import setup_parser, SdxlNetworkTrainer
-from library.train_util import read_config_from_file
-
 try:
+    from sdxl_train_network import setup_parser, SdxlNetworkTrainer
+    from library.train_util import read_config_from_file
+
     parser = setup_parser()
     args = parser.parse_args()
     args = read_config_from_file(args, parser)
+
     trainer = SdxlNetworkTrainer()
     trainer.train(args)
+
 except:
-    print(traceback.format_exc())
+    import sys
+    import traceback
+    from pygments import formatters, highlight, lexers
+    from dracula import DraculaStyle
+
+    tb = traceback.format_exc().split("\n")
+    tb_text = "\n".join(tb[:-2])
+
+    lexer = lexers.get_lexer_by_name("pytb", stripall=True)
+    formatter = formatters.Terminal256Formatter(style=DraculaStyle)
+    tb_colored = highlight(tb_text, lexer, formatter)
+
+    print(f"{tb_colored}\n\033[0;31m\033[1m{tb[-2]}\n")
     sys.exit(1)
